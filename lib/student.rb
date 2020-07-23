2441a5571a378d8a46b2eae2e5e960c00361e77b
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-23 23:31:38 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,12 @@
-class Student 
+class Student < User
+  attr_accessor :first_name, :last_name, :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+  
+  def learn(knowledge)
+    @knowledge << knowledge
+  end
 
 end
